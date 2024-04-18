5eba4148d5d668672200fbd40c50da2031ce665d
Hwang <102862313+Hwang9170@users.noreply.github.com>
2024-04-19 00:05:55 +0900

    커피 사먹고 다시 함

--- test (독학)/정규.py
+++ test (독학)/정규.py
@@ -38,8 +38,73 @@ for star in solar:
   print("%s은 %d번째 행성"%(star,count))
   count = count+1
 
+for count in range(5,10):
+  print("안녕 count = ",count)
+print("----------")
+for count in range(3,15,3):
+  print("안녕 count = ",count)
+print("-----")
+
+for i in range(5,10):
+  print(f"변수의 i의 값={i}")
+print()
+for i in range(1,10,3):
+  print(f"변수의 i의 값={i}")
+print()
+
+for i in range(10,7,-1):
+  print(f"변수의 i의 값={i}")
+print()
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+radius =100
+t.circle(radius)
+t.fd(50)
+radius = radius+50
+t.circle(radius)
+t.fd(50)
+radius = radius+50
+t.circle(radius)
+t.fd(50)
+radius = radius+50
+t.circle(radius)
+
+foodlist = []
+food = input("음식이름>>")
+foodlist.append(food)
+food = input("음식이름>>")
+foodlist.append(food)
+food = input("음식이름>>")
+foodlist.append(food)
+food = input("음식이름>>")
+foodlist.append(food)
+print(foodlist)
+
+for i in range(6):
+ t.circle(100)
+ t.left(60)
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+clist = ["red","green","blue","yellow"]
 
 
+for i in range(4):
+  t.fillcolor(clist[i])
+  t.begin_fill()
+  t.circle(100)
+  t.end_fill()
+  t.fd(100)
 
+number = turtle.textinput("","몇각형을 원하시나요?")
+number = int(number)
 
-
+for i in range(number):
+  t.fd(100)
+  t.left(360/number)
+t.done()
